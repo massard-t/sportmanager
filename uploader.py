07252070bfe5741217ff222b607b0cac7533e572
Theo Massard <massar_t@etna-alternance.net>
2017-10-02 00:16:08 +0200

    Create workout type, still need to upload to trello

--- uploader.py
+++ uploader.py
@@ -4,25 +4,37 @@
 Synchronizes workouts in the ./data dir with the Trello board
 """
 import os
+from collections import namedtuple
 
 __author__ = 'Theo Massard <massar_t@etna-alternance.net>'
 
+
 TYPES = ('push', 'pull', 'leg')
+Workout = namedtuple('Workout', ['w_type', 'w_day', 'w_name', 'w_data'])
 
 
 def get_workout_type(fname):
+    """Associates a filename to a workout type"""
     for workout_type in TYPES:
         if workout_type in fname:
-            return (workout_type, fname)
+            content = open(fname, 'r').read().split('\n')
+            print(content)
+            workout = Workout(
+                w_type=workout_type,
+                w_day=content[0],
+                w_name=workout_type+fname.replace(workout_type, ''),
+                w_data='\n'.join(content[1:])
+                )
+            return workout
 
 
-def find_workouts(directory='./data'):
+def find_workouts(directory='./data/'):
     """Returns a list of tuples
 
     (workout_type, workout_file)
     """
     for fname in os.listdir(directory):
-        print(get_workout_type(fname))
+        print(get_workout_type(directory+fname))
 
 
 def main():
